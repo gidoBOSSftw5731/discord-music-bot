c0345f147bb2fe7e3dfb62ccf83532fadc299a6a
gidoBOSSftw5731 <celtictarot386@gmail.com>
2020-05-11 11:23:31 -0400

    remove quality settings it mega broke

--- main.go
+++ main.go
@@ -377,12 +377,12 @@ func dlToTmp(url string) (string, error) {
 	youtubeDl.Options.ExtractAudio.Value = true
 	youtubeDl.Options.AudioFormat.Value = "mp3"
 	youtubeDl.Options.KeepVideo = goydl.BoolOption{Value: false} // why is this a thing
-	youtubeDl.Options.AudioQuality = goydl.StringOption{Value: "0"}
+//	youtubeDl.Options.AudioQuality = goydl.StringOption{Value: "0"}
 
 	youtubeDl.VideoURL = fmt.Sprintf("www.youtube.com/watch?v=%v", url)
 	// listen to errors from ydl
-	//go io.Copy(os.Stdout, youtubeDl.Stdout)
-	//go io.Copy(os.Stderr, youtubeDl.Stderr)
+//	go io.Copy(os.Stdout, youtubeDl.Stdout)
+//	go io.Copy(os.Stderr, youtubeDl.Stderr)
 
 	dwnld, err := youtubeDl.Download()
 	if err != nil {
