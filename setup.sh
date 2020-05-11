99ca8fc575b548f3d06468abeeb4edad726a406d
gido5731 <celtictarot386@gmail.com>
2020-05-11 02:40:08 -0400

    work on config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+touch config.yml
+chown logname config.yml
+
 #install stuff for ffpeg
 apt -y install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev libavdevice-dev libavfilter-dev libswscale-dev libavcodec-dev libavformat-dev libswresample-dev libavutil-dev yasm
 #prep for ffmpeg
